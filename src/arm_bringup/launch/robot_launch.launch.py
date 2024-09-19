7af7874a4bfe0319eba23b6d2225d3994fe1be14
sssecs <ssssssecs@gmail.com>
2024-09-19 11:11:38 +0800

    tidy up launch

--- src/arm_bringup/launch/robot_launch.launch.py
+++ src/arm_bringup/launch/robot_launch.launch.py
@@ -16,18 +16,17 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
 
-
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
-
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('arm_bringup'),'launch','rsp.launch.py'
                 )]), launch_arguments={'use_sim_time': 'false'}.items()
     )
 
-    
-
+    moveit = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('arm_moveit_config'),'launch','move_group.launch.py'
+                )]), launch_arguments={'use_sim_time': 'false'}.items()
+    )
 
     robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
 
@@ -68,29 +67,10 @@ def generate_launch_description():
         )
     )
 
-
-    # Code for delaying a node (I haven't tested how effective it is)
-    # 
-    # First add the below lines to imports
-    # from launch.actions import RegisterEventHandler
-    # from launch.event_handlers import OnProcessExit
-    #
-    # Then add the following below the current diff_drive_spawner
-    # delayed_diff_drive_spawner = RegisterEventHandler(
-    #     event_handler=OnProcessExit(
-    #         target_action=spawn_entity,
-    #         on_exit=[diff_drive_spawner],
-    #     )
-    # )
-    #
-    # Replace the diff_drive_spawner in the final return with delayed_diff_drive_spawner
-
-
-
-    # Launch them all!
     return LaunchDescription([
         rsp,
         delayed_controller_manager,
         delayed_joint_trajectory_spawner,
-        delayed_joint_broad_spawner
+        delayed_joint_broad_spawner,
+        moveit
     ])
--- src/arm_bringup/launch/sim_launch.launch.py
+++ src/arm_bringup/launch/sim_launch.launch.py
@@ -13,25 +13,24 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
 
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
-
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory("arm_bringup"),'launch','rsp.launch.py'
                 )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
-    # gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
+    moveit = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('arm_moveit_config'),'launch','move_group.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
 
-    # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
                     # launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
              )
 
-    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
                         arguments=['-topic', 'robot_description',
                                    '-entity', 'arm'],
@@ -49,13 +48,11 @@ def generate_launch_description():
         arguments=["joint_state_broadcaster"],
     )
 
-
-
-    # Launch them all!
     return LaunchDescription([
         rsp,
         gazebo,
         spawn_entity,
         joint_trajectory_spawner,
-        joint_broad_spawner
+        joint_broad_spawner,
+        moveit
     ])
